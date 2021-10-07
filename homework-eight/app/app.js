d7a1eb038ece53ccf1a7a13be171eb92e6b02ca2
matthewmasko <90282407+matthewmasko@users.noreply.github.com>
2021-10-07 07:44:29 -0400

    Initial commit for the Homework 8 assignment for 215.

---
+++ homework-eight/app/app.js
@@ -0,0 +1,163 @@
+var homeContent = `<div class="home">
+<h1>HOME</h1>
+<div class="imageContainerHome">
+    <img src="images/apple-5ave.jpeg" alt="Apple 5th Avenue, NYC">
+</div>
+<p>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur consequatur nobis quod ipsum placeat quidem neque in aut debitis, quasi explicabo vel veritatis, assumenda maiores vero suscipit voluptatem esse porro! Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sit illo fugit deleniti vero, sunt similique mollitia corporis autem molestias exercitationem vel excepturi amet. Libero dolorum esse quo sequi excepturi.
+</p>
+<p>
+Quas aut iure doloribus. Ipsa necessitatibus sapiente molestias quidem quos expedita aliquam possimus sequi aperiam eligendi commodi dolores consectetur asperiores eius, atque ex recusandae enim itaque vel sint nostrum? Iure.
+</p>
+<p>
+Minus atque praesentium voluptatem distinctio ducimus unde labore molestiae ex doloribus, quos saepe, dolore exercitationem! Explicabo, repellendus sint ipsam repellat neque saepe iste. Quasi, beatae id vitae dicta explicabo vel.
+</p>
+</div>
+
+<div class="home">
+<div class="imageContainerHome">
+<img src="images/apple-gcc.jpeg" alt="Apple Grand Central Station, NYC">
+</div>
+<p>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur consequatur nobis quod ipsum placeat quidem neque in aut debitis, quasi explicabo vel veritatis, assumenda maiores vero suscipit voluptatem esse porro! Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sit illo fugit deleniti vero, sunt similique mollitia corporis autem molestias exercitationem vel excepturi amet. Libero dolorum esse quo sequi excepturi.
+</p>
+<p>
+Quas aut iure doloribus. Ipsa necessitatibus sapiente molestias quidem quos expedita aliquam possimus sequi aperiam eligendi commodi dolores consectetur asperiores eius, atque ex recusandae enim itaque vel sint nostrum? Iure.
+</p>
+<p>
+Minus atque praesentium voluptatem distinctio ducimus unde labore molestiae ex doloribus, quos saepe, dolore exercitationem! Explicabo, repellendus sint ipsam repellat neque saepe iste. Quasi, beatae id vitae dicta explicabo vel.
+</p>
+</div>
+
+<div class="home">
+<div class="imageContainerHome">
+<img src="images/apple-dog.jpeg" alt="Apple SoHo, NYC">
+</div>
+<p>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur consequatur nobis quod ipsum placeat quidem neque in aut debitis, quasi explicabo vel veritatis, assumenda maiores vero suscipit voluptatem esse porro! Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sit illo fugit deleniti vero, sunt similique mollitia corporis autem molestias exercitationem vel excepturi amet. Libero dolorum esse quo sequi excepturi.
+</p>
+<p>
+Quas aut iure doloribus. Ipsa necessitatibus sapiente molestias quidem quos expedita aliquam possimus sequi aperiam eligendi commodi dolores consectetur asperiores eius, atque ex recusandae enim itaque vel sint nostrum? Iure.
+</p>
+<p>
+Minus atque praesentium voluptatem distinctio ducimus unde labore molestiae ex doloribus, quos saepe, dolore exercitationem! Explicabo, repellendus sint ipsam repellat neque saepe iste. Quasi, beatae id vitae dicta explicabo vel.
+</p>
+</div>`;
+
+var aboutContent = ` <div class="about">
+<h1>ABOUT</h1>
+<div class="imageContainer">
+   <img src="images/masko-apple5.jpeg" alt="Masko at Apple 5th Ave., NYC">
+   <img src="images/masko-apple-gcc.jpeg" alt="Masko at Apple, Grand Central Station">
+   <img src="images/masko-apple-SoHo.jpeg" alt="Masko at Apple, SoHo">
+</div>
+<p>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur consequatur nobis quod ipsum placeat quidem neque in aut debitis, quasi explicabo vel veritatis, assumenda maiores vero suscipit voluptatem esse porro! Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sit illo fugit deleniti vero, sunt similique mollitia corporis autem molestias exercitationem vel excepturi amet. Libero dolorum esse quo sequi excepturi. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis cum incidunt voluptatem commodi, veniam consectetur perspiciatis fugit suscipit rerum necessitatibus magni. Accusantium, laboriosam animi optio consequuntur sit provident vero quaerat!
+</p>
+<p>
+Quas aut iure doloribus. Ipsa necessitatibus sapiente molestias quidem quos expedita aliquam possimus sequi aperiam eligendi commodi dolores consectetur asperiores eius, atque ex recusandae enim itaque vel sint nostrum? Iure. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae, dicta tenetur nesciunt, odit eum numquam et quisquam iste velit aut beatae obcaecati laboriosam consequatur, necessitatibus quos? Repudiandae quis sequi ad.
+</p>
+<p>
+Minus atque praesentium voluptatem distinctio ducimus unde labore molestiae ex doloribus, quos saepe, dolore exercitationem! Explicabo, repellendus sint ipsam repellat neque saepe iste. Quasi, beatae id vitae dicta explicabo vel.
+</p>
+<p>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur consequatur nobis quod ipsum placeat quidem neque in aut debitis, quasi explicabo vel veritatis, assumenda maiores vero suscipit voluptatem esse porro! Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sit illo fugit deleniti vero, sunt similique mollitia corporis autem molestias exercitationem vel excepturi amet. Libero dolorum esse quo sequi excepturi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit dolor quia soluta, nam fuga doloremque porro sequi officiis similique placeat numquam recusandae, omnis sit corrupti unde, suscipit atque impedit eum.
+</p>
+<p>
+Quas aut iure doloribus. Ipsa necessitatibus sapiente molestias quidem quos expedita aliquam possimus sequi aperiam eligendi commodi dolores consectetur asperiores eius, atque ex recusandae enim itaque vel sint nostrum? Iure. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat quos enim velit modi rem doloribus natus saepe eius architecto. Optio velit veritatis totam cupiditate quod molestias enim laudantium est modi.
+</p>
+<p>
+Minus atque praesentium voluptatem distinctio ducimus unde labore molestiae ex doloribus, quos saepe, dolore exercitationem! Explicabo, repellendus sint ipsam repellat neque saepe iste. Quasi, beatae id vitae dicta explicabo vel.
+</p>
+<p>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur consequatur nobis quod ipsum placeat quidem neque in aut debitis, quasi explicabo vel veritatis, assumenda maiores vero suscipit voluptatem esse porro! Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sit illo fugit deleniti vero, sunt similique mollitia corporis autem molestias exercitationem vel excepturi amet. Libero dolorum esse quo sequi excepturi.
+</p>
+<p>
+Quas aut iure doloribus. Ipsa necessitatibus sapiente molestias quidem quos expedita aliquam possimus sequi aperiam eligendi commodi dolores consectetur asperiores eius, atque ex recusandae enim itaque vel sint nostrum? Iure.
+</p>
+<p>
+Minus atque praesentium voluptatem distinctio ducimus unde labore molestiae ex doloribus, quos saepe, dolore exercitationem! Explicabo, repellendus sint ipsam repellat neque saepe iste. Quasi, beatae id vitae dicta explicabo vel.
+</p>
+</div>
+`;
+var projectContent = `<div class="project">
+<h1>PROJECT</h1>
+<div class="imageContainer">
+   <img src="images/los-tacos1.jpeg" alt="Los Tacos Number 1">
+   <img src="images/ghostbusters.jpeg" alt="Dana Barrett's apartment building and Holy Trinity Lutheran Church">
+   <img src="images/bryant.jpeg" alt="Bryant Park">
+</div>
+<div class="imageContainer">
+    <img src="images/central-park.jpeg" alt="Central Park">
+    <img src="images/UN.jpeg" alt="Outside the United Nations headquarters">
+    <img src="images/911.jpeg" alt="9/11 Memorial, south tower, NYC">
+ </div>
+<p>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur consequatur nobis quod ipsum placeat quidem neque in aut debitis, quasi explicabo vel veritatis, assumenda maiores vero suscipit voluptatem esse porro! Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sit illo fugit deleniti vero, sunt similique mollitia corporis autem molestias exercitationem vel excepturi amet. Libero dolorum esse quo sequi excepturi. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis cum incidunt voluptatem commodi, veniam consectetur perspiciatis fugit suscipit rerum necessitatibus magni. Accusantium, laboriosam animi optio consequuntur sit provident vero quaerat!
+</p>
+<p>
+Quas aut iure doloribus. Ipsa necessitatibus sapiente molestias quidem quos expedita aliquam possimus sequi aperiam eligendi commodi dolores consectetur asperiores eius, atque ex recusandae enim itaque vel sint nostrum? Iure. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae, dicta tenetur nesciunt, odit eum numquam et quisquam iste velit aut beatae obcaecati laboriosam consequatur, necessitatibus quos? Repudiandae quis sequi ad.
+</p>
+<p>
+Minus atque praesentium voluptatem distinctio ducimus unde labore molestiae ex doloribus, quos saepe, dolore exercitationem! Explicabo, repellendus sint ipsam repellat neque saepe iste. Quasi, beatae id vitae dicta explicabo vel.
+</p>
+<p>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur consequatur nobis quod ipsum placeat quidem neque in aut debitis, quasi explicabo vel veritatis, assumenda maiores vero suscipit voluptatem esse porro! Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sit illo fugit deleniti vero, sunt similique mollitia corporis autem molestias exercitationem vel excepturi amet. Libero dolorum esse quo sequi excepturi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit dolor quia soluta, nam fuga doloremque porro sequi officiis similique placeat numquam recusandae, omnis sit corrupti unde, suscipit atque impedit eum.
+</p>
+<p>
+Quas aut iure doloribus. Ipsa necessitatibus sapiente molestias quidem quos expedita aliquam possimus sequi aperiam eligendi commodi dolores consectetur asperiores eius, atque ex recusandae enim itaque vel sint nostrum? Iure. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat quos enim velit modi rem doloribus natus saepe eius architecto. Optio velit veritatis totam cupiditate quod molestias enim laudantium est modi.
+</p>
+<p>
+Minus atque praesentium voluptatem distinctio ducimus unde labore molestiae ex doloribus, quos saepe, dolore exercitationem! Explicabo, repellendus sint ipsam repellat neque saepe iste. Quasi, beatae id vitae dicta explicabo vel.
+</p>
+<p>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur consequatur nobis quod ipsum placeat quidem neque in aut debitis, quasi explicabo vel veritatis, assumenda maiores vero suscipit voluptatem esse porro! Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sit illo fugit deleniti vero, sunt similique mollitia corporis autem molestias exercitationem vel excepturi amet. Libero dolorum esse quo sequi excepturi.
+</p>
+<p>
+Quas aut iure doloribus. Ipsa necessitatibus sapiente molestias quidem quos expedita aliquam possimus sequi aperiam eligendi commodi dolores consectetur asperiores eius, atque ex recusandae enim itaque vel sint nostrum? Iure.
+</p>
+<p>
+Minus atque praesentium voluptatem distinctio ducimus unde labore molestiae ex doloribus, quos saepe, dolore exercitationem! Explicabo, repellendus sint ipsam repellat neque saepe iste. Quasi, beatae id vitae dicta explicabo vel.
+</p>
+</div>`
+var contactContent = `<div class="contact">
+<h1>CONTACT</h1>
+<div class="contactContainer">
+<form>
+  <div class="textInputs">
+    <label for="firstName">first name:</label>
+    <input type="text" id="firstName" />
+  </div>
+  <div class="textInputs">
+    <label for="lastName">last name:</label>
+    <input type="text" id="lastName" />
+  </div>
+  <div class="textInputs">
+    <label for="email">email:</label>
+    <input type="text" id="email" />
+  </div>
+  <div class="textInputsComments">
+    <label for="commentsQuestions">comments/questions:</label>
+    <input type="text" id="commentsQuestions" />
+  </div>
+  <div class="submitButton">
+    <input type="submit" class="subBtn" value="submit" />
+  </div>
+</form>
+</div>
+// </div>`;
+
+
+function initListeners() {
+    $("nav a").click(function(e) {
+        // this stores the id from the anchor tag.
+let aID = e.currentTarget.id;
+let contentID = aID + "Content";
+    $("#app").html(eval(contentID));
+});
+}
+
+$(document).ready(function() {
+  initListeners();
+  $("#app").html(homeContent);
+});
